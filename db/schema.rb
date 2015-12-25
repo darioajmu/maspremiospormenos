70f44b06fe412a5ea294536fdc3864aa88fd3772
dario.ajmu <dario.ajmu@gmail.com>
2015-12-25 15:51:21 +0100

    campos de usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222190535) do
+ActiveRecord::Schema.define(version: 20151225144117) do
 
   create_table "tiposdocumentos", force: :cascade do |t|
     t.datetime "created_at",              null: false
@@ -22,12 +22,12 @@ ActiveRecord::Schema.define(version: 20151222190535) do
   end
 
   create_table "usuarios", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255,                default: "", null: false
+    t.string   "encrypted_password",     limit: 255,                default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,                  default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
@@ -40,6 +40,17 @@ ActiveRecord::Schema.define(version: 20151222190535) do
     t.string   "confirmation_token",     limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "nombre",                 limit: 255
+    t.string   "apellido1",              limit: 255
+    t.string   "apellido2",              limit: 255
+    t.integer  "provincia_id",           limit: 4
+    t.string   "ciudad",                 limit: 255
+    t.decimal  "codigopostal",                       precision: 10
+    t.string   "direccion",              limit: 255
+    t.string   "direccioncompleta",      limit: 255
+    t.date     "fechadenacimiento"
+    t.string   "sexo",                   limit: 255
+    t.decimal  "movil",                              precision: 10
   end
 
   add_index "usuarios", ["confirmation_token"], name: "index_usuarios_on_confirmation_token", unique: true, using: :btree
