fe773809aca19384931dc47b16478dda70aeca37
dario.ajmu <dario.ajmu@gmail.com>
2015-12-25 16:09:05 +0100

    añado campos de devise en el application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :tiposdocumento_id, :documento) }
-        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :current_password, :tiposdocumento_id, :documento) }
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :tiposdocumento_id, :documento, :username, :nombre, :apellido1, :apellido2, :provincia_id, :ciudad, :codigopostal, :direccion, :direccioncompleta, :fechadenacimiento, :movil, :sexo) }
+        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :current_password, :provincia_id, :ciudad, :codigopostal, :direccion, :direccioncompleta, :fechadenacimiento, :movil) }
     end
 
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -5,6 +5,18 @@ class Usuario < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :tiposdocumento_id, presence: true
+  validates :username, presence: true
   validates :documento, presence: true
+  validates :nombre, presence: true
+  validates :apellido1, presence: true
+  validates :apellido2, presence: true
+  validates :provincia_id, presence: true
+  validates :ciudad, presence: true
+  validates :codigopostal, presence: true
+  validates :direccion, presence: true
+  validates :fechadenacimiento, presence: true
+  validates :movil, presence: true
+  validates :sexo, presence: true
+
 
 end
