5d73f302867b00978fd88de9829b9f6efafd3e99
dario.ajmu <dario.ajmu@gmail.com>
2015-12-29 20:16:36 +0100

    solicitar cambio de contraseña tanto por email como por username

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -7,7 +7,7 @@ class Usuario < ActiveRecord::Base
 
   validates :tiposdocumento_id, presence: true
   validates :username, presence: true, :uniqueness => {:case_sensitive => false}
-  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true 
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
   validates :documento, presence: true
   validates :nombre, presence: true
   validates :apellido1, presence: true
@@ -20,12 +20,16 @@ class Usuario < ActiveRecord::Base
   validates :movil, presence: true
   validates :sexo, presence: true
 
-  def self.find_for_database_authentication(warden_conditions)
+  def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
     else
-      where(conditions.to_hash).first
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
     end
   end
 
