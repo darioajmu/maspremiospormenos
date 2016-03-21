b57307f2603cd8201f55c123c0e3849c082db9d4
dajdadi <dajdadi@qipert.com>
2016-03-21 20:19:23 +0100

    editar datos de usuario

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,3 +1,25 @@
 class UsuariosController < ApplicationController
+  before_action :objeto_find, only: [:edit, :show, :update, :destroy]
 
+def edit
+end
+
+def update
+  if @usuario.update(objeto_parametros)
+    redirect_to 'index'
+  else
+    render 'edit'
+  end
+end
+
+private
+def objeto_find
+	@usuario = Usuario.find(params[:id])
+end
+
+def objeto_parametros
+	params.require(:usuario).permit(:username, :email, :encrypted_password, :password, :apellido1,
+	 :apellido2, :ciudad, :codigopostal, :nombre, :direccion, :direccioncompleta, :codigo, :documento,
+ 	 :fechadenacimiento, :movil, :provincia_id, :sexo, :tiposdocumento_id, :password_confirmation)
+end
 end
