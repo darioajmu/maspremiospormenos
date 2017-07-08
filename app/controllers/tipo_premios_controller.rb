34765bb22385eac1b94b157432ad10b6dbb7ca64
Dario Ajdadi <dario.ajmu@gmail.com>
2017-07-08 12:53:51 +0200

    Changed routes and fixed views and CRUD

--- app/controllers/tipo_premios_controller.rb
+++ app/controllers/tipo_premios_controller.rb
@@ -6,4 +6,23 @@ class TipoPremiosController < ApplicationController
   def new
     @tipo_premio = TipoPremio.new
   end
+
+  def create
+    @tipo_premio = TipoPremio.new(tipo_premio_params)
+    respond_to do |format|
+      if @tipo_premio.save
+        format.html { redirect_to tipo_premios_path, notice: 'Business KPI was successfully created.' }
+        format.json { render :show, status: :created, location: @tipo_premio }
+      else
+        format.html { render :new }
+        format.json { render json: @tipo_premio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def tipo_premio_params
+    params.require(:tipo_premio).permit(:nombre, :codigo)
+  end
 end
