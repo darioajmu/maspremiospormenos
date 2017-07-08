e7efbed4d114ab9dceda8313f648faadf1a06f82
Dario Ajdadi <dario.ajmu@gmail.com>
2017-07-08 13:13:08 +0200

    Uniqueness scoped by type

--- spec/models/simple_object_spec.rb
+++ spec/models/simple_object_spec.rb
@@ -2,19 +2,7 @@ require 'rails_helper'
 
 RSpec.describe SimpleObject, type: :model do
   it { is_expected.to validate_presence_of :codigo }
-  it { is_expected.to validate_uniqueness_of :codigo }
+  it { is_expected.to validate_uniqueness_of(:codigo).scoped_to(:type) }
   it { is_expected.to validate_presence_of :nombre }
-  it { is_expected.to validate_uniqueness_of :nombre }
+  it { is_expected.to validate_uniqueness_of(:nombre).scoped_to(:type) }
 end
-
-# == Schema Information
-#
-# Table name: simple_objects
-#
-#  id         :integer          not null, primary key
-#  type       :string
-#  codigo     :string
-#  nombre     :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
