117ac41e49249caa90a965bc6fabb2118246d1f0
Dario Ajdadi <dario.ajmu@gmail.com>
2017-07-08 10:50:05 +0200

    Annotated models, and deleted test models

--- app/models/provincia.rb
+++ app/models/provincia.rb
@@ -1,2 +1,13 @@
 class Provincia < ActiveRecord::Base
 end
+
+# == Schema Information
+#
+# Table name: provincias
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  codigo     :string
+#  nombre     :string
+#
--- app/models/tipo_documento.rb
+++ app/models/tipo_documento.rb
@@ -1,2 +1,14 @@
 class TipoDocumento < SimpleObject
 end
+
+# == Schema Information
+#
+# Table name: simple_objects
+#
+#  id         :integer          not null, primary key
+#  type       :string
+#  codigo     :string
+#  nombre     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
