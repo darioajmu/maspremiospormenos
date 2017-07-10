709f87b00c5d7c773e397d0104c812df3417eac1
Dario Ajdadi <dario.ajmu@gmail.com>
2017-07-10 10:57:24 +0200

    Front controll premios

--- app/helpers/premios_helper.rb
+++ app/helpers/premios_helper.rb
@@ -2,4 +2,8 @@ module PremiosHelper
   def set_missing_participations(premio)
     (premio.numero_participaciones) - (Sorteo.where(premio_id: premio.id).size)
   end
+
+  def link_to_assign(premio)
+    set_missing_participations(premio) > 0 ? "link_to 'Añadir', assign_participation_sorteos_path(premio_id: premio.id, usuario_id: current_usuario.id)" : ""
+  end
 end
